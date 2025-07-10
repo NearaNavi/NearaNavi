45189f0cde814bb24d0dcfb0caf2b64e0de5af7f
NearaNavi <kelli.johnson@nearanavi.com>
2025-07-10 11:47:17 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to NearaNavi" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Shining a light on the local businesses you won't find anywhere else. <code>pages/index.js</code>
         </p>
       </main>
 
