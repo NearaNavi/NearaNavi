daad50f6c5ec00f141305a014fbae6ad5c340256
NearaNavi <kelli.johnson@nearanavi.com>
2025-07-10 13:17:13 -0500

    Update add.js

--- pages/add.js
+++ pages/add.js
@@ -1,1 +1,9 @@
+export default function AddBusiness() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Add Your Business</h1>
+      <p>This is where your business submission form will go.</p>
+    </div>
+  );
+}
 
