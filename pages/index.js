a47d2d8e99208e68098f252e8b3cb54496641ec0
NearaNavi <kelli.johnson@nearanavi.com>
2025-07-10 19:44:25 -0500

    Update index.js
    chore: remove diff markers from index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 
-+ import Head from 'next/head'
-+ import Header from '@components/Header'
-+ import Footer from '@components/Footer'
-
+import Head   from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
 export default function Home() {
   return (
     <div className="container">
