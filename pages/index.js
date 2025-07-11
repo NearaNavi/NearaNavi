414566ca191440b8b7c45c3018dd9fd7dc5eec5f
NearaNavi <kelli.johnson@nearanavi.com>
2025-07-10 19:40:33 -0500

    Update index.js
    Remove duplicate imports from index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 
-import Header from '@components/Header'
-import Head   from 'next/head'
-import Footer from '@components/Footer'
++ import Head from 'next/head'
++ import Header from '@components/Header'
++ import Footer from '@components/Footer'
+
 export default function Home() {
   return (
     <div className="container">
